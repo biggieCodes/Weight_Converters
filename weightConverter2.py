7adb5954c46473de51b0ba007a18a7cef6783385
Abdulsamad Rabo <raboabdulsamad04@gmail.com>
2023-09-29 13:01:15 +0100

    second

--- weightConverter2.py
+++ weightConverter2.py
@@ -7,7 +7,7 @@ def from_kg():
     gram  = float(e2_value.get())*1000
     pound = float(e2_value.get())*2.20462
     ounce = float(e2_value.get())*35.274
-    
+    tonnes = float(e2_value.get())*2000
     
     t1.delete("1.0",END)
     t1.insert(END, gram)
@@ -15,8 +15,12 @@ def from_kg():
     t2.insert(END, pound)
     t3.delete("1.0", END)
     t3.insert(END, ounce)
-   # t4.delete("1.0", END)
-   # t4.insert(END, tonne)
+    t4.delete("1.0", END)
+    t4.insert(END, tonnes)
+    t5.delete("1.0", END)
+    t5.insert(END, tonnes)
+    t6.delete("1.0", END)
+    t6.insert(END, tonnes)
     
 
     
@@ -27,14 +31,14 @@ e2 = Entry(window, textvariable=e2_value)
 e3 = Label(window, text="Gram")
 e4 = Label(window, text="Pound")
 e5 = Label(window, text="Ounce")
-#e6 = Label(window,text="Tonne")
+e6 = Label(window,text="Tonne")
 
 t1 = Text(window, height=5, width=30)
 t2 = Text(window, height=5, width=30)
 t3 = Text(window, height=5, width=30)
 t4 = Text(window, height=5, width=30)
 t5 = Text(window, height=5, width=30)
-#t6 = Text(window, height=5, width=30)
+t6 = Text(window, height=5, width=30)
 
 
 b1 = Button(window, text="Convert",width=10,activebackground= "blue",activeforeground= "grey", command=from_kg)
@@ -44,14 +48,14 @@ e2.grid(row=0, column=1)
 e3.grid(row=1, column=0)
 e4.grid(row=1, column=1)
 e5.grid(row=1, column=2)
-#e6.grid(row=1, column=2)
+e6.grid(row=1, column=3)
 
 t1.grid(row=2, column=0)
 t2.grid(row=2, column=1)
 t3.grid(row=2, column=2)
-# t4.grid(row=2, column=2)
-# t5.grid(row=2, column=2)
-#t6.grid(row=2, column=2)
+t4.grid(row=2, column=3)
+#t5.grid(row=2, column=4)
+#t6.grid(row=2, column=5)
 
 b1.grid(row=0, column=2)
 
